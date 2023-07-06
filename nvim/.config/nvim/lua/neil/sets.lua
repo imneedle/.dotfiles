8fa3cc0f628943075ae25fc88c7673602e86c5aa
imneedle <77881374+imneedle@users.noreply.github.com>
2023-07-06 16:58:21 -0500

    feat: nvim added global sets

---
+++ nvim/.config/nvim/lua/neil/sets.lua
@@ -0,0 +1,40 @@
+-- Vim display
+vim.opt.signcolumn  = "yes"
+vim.opt.cursorline  = true
+vim.opt.colorcolumn = "80"
+
+-- User interface
+vim.opt.termguicolors = true
+vim.opt.wrap          = false
+vim.opt.foldenable    = false
+vim.opt.list          = true
+vim.opt.listchars:append("eol:↴")
+vim.opt.listchars:append("tab:> ")
+vim.opt.listchars:append("trail:·")
+vim.opt.listchars:append("nbsp:⎵")
+
+-- Line numbers
+vim.opt.number         = true
+vim.opt.relativenumber = true
+
+-- Temporary directories
+vim.opt.swapfile = true
+vim.opt.undodir  = vim.fn.stdpath "data" .. "/undo"
+vim.opt.undofile = true
+vim.opt.backup   = false
+
+-- Window splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Scrolloff
+vim.opt.scrolloff     = 6
+vim.opt.sidescrolloff = 4
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase  = true
+vim.opt.hlsearch   = false
+
+-- Misc options
+vim.opt.confirm = true
