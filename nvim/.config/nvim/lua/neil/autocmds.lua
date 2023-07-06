68a55e756310c771037325d122e0f1756dcad864
imneedle <77881374+imneedle@users.noreply.github.com>
2023-07-06 16:58:30 -0500

    feat: nvim added global augroups

---
+++ nvim/.config/nvim/lua/neil/autocmds.lua
@@ -0,0 +1,29 @@
+-- Highlight on yank
+local highlightYankGroup = vim.api.nvim_create_augroup("HighlightYank", {
+    clear = true
+})
+vim.api.nvim_create_autocmd("TextYankPost", {
+    command = "silent! lua vim.highlight.on_yank()",
+    group = highlightYankGroup,
+})
+
+-- Better comments with format options
+local betterFormatOptionsGroup = vim.api.nvim_create_augroup(
+    "BetterFormatOptions",
+    { clear = true }
+)
+-- Disable auto comment wrapping when line exceeds 80 chars
+vim.api.nvim_create_autocmd("FileType", {
+    command = "silent! set formatoptions-=c",
+    group = betterFormatOptionsGroup,
+})
+-- Disable auto inserting comment leader when pressing <CR> (enter) on comment
+vim.api.nvim_create_autocmd("FileType", {
+    command = "silent! set formatoptions-=r",
+    group = betterFormatOptionsGroup,
+})
+-- Disable auto inserting comment leader when pressing "o" or "O" on comment
+vim.api.nvim_create_autocmd("FileType", {
+    command = "silent! set formatoptions-=o",
+    group = betterFormatOptionsGroup,
+})
