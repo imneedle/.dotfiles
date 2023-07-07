e7d6f89264676ff4a34acdfe114cf147d15d42ed
imneedle <77881374+imneedle@users.noreply.github.com>
2023-07-06 20:54:29 -0500

    feat: nvim added onedarkpro colorscheme plugin

---
+++ nvim/.config/nvim/lua/neil/plugins/onedarkpro.lua
@@ -0,0 +1,8 @@
+return {
+    "olimorris/onedarkpro.nvim",
+    lazy = false, -- Make sure we load this plugin during startup
+    priority = 1000, -- Make sure we load this plugin before all other plugins
+    config = function() -- Set the colorscheme on load
+      vim.cmd("colorscheme onedark")
+    end,
+}
