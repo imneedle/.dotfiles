5cdbf1ea6a76556b949de868e7ee529232f83e35
imneedle <77881374+imneedle@users.noreply.github.com>
2023-07-07 14:54:52 -0500

    feat: nvim added telescope plugin

---
+++ nvim/.config/nvim/lua/neil/plugins/telescope.lua
@@ -0,0 +1,56 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    lazy = false,
+    tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        {
+            "<leader>pf",
+            ":lua require('telescope.builtin').find_files({ hidden = true })<CR>"
+        },
+        {
+            "<leader>pg",
+            ":lua require('telescope.builtin').live_grep()<CR>"
+        },
+        {
+            "<leader>pb",
+            ":lua require('telescope.builtin').buffers()<CR>"
+        },
+        {
+            "<leader>ph",
+            ":lua require('telescope.builtin').help_tags()<CR>"
+        },
+        {
+            "<leader>pq",
+            ":lua require('telescope.builtin').quickfix()<CR>"
+        },
+        {
+            "<leader>pr",
+            ":lua require('telescope.builtin').registers()<CR>"
+        },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require "telescope".setup{
+            defaults = {
+                sorting_strategy = "ascending",
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        mirror = false,
+                        width = .8,
+                        prompt_position = "top",
+                        preview_width = 80
+                    }
+                },
+                mappings = {
+                    i = {
+                        ["<Tab>"] = actions.select_tab, -- open in a new tab
+                        ["<S-Tab>"] = actions.select_tab, -- open in a new tab
+                        ["<Esc>"] = actions.close -- quit
+                    }
+                }
+            }
+        }
+    end,
+}
