2648ed32be028be4a5382db2c0989df12a83df11
imneedle <77881374+imneedle@users.noreply.github.com>
2023-08-04 13:20:47 -0500

    fix: harpoon lazy load key remaps now working:
    (had to remove the "<silent>" in the keymap)

--- nvim/.config/nvim/lua/neil/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/neil/plugins/harpoon.lua
@@ -5,14 +5,14 @@ return {
         { "nvim-lua/plenary.nvim" },
     },
     keys = {
-        { "<silent><leader>ha", ":lua require 'harpoon.mark'.add_file()<CR>" },
+        { "<leader>ha", ":lua require 'harpoon.mark'.add_file()<CR>" },
         {
-            "<silent><leader>hm",
+            "<leader>hm",
             ":lua require 'harpoon.ui'.toggle_quick_menu()<CR>",
         },
-        { "<silent><C-h>", ":lua require 'harpoon.ui'.nav_file(1)<CR>" },
-        { "<silent><C-t>", ":lua require 'harpoon.ui'.nav_file(2)<CR>" },
-        { "<silent><C-n>", ":lua require 'harpoon.ui'.nav_file(3)<CR>" },
-        { "<silent><C-s>", ":lua require 'harpoon.ui'.nav_file(4)<CR>" },
+        { "<C-h>", ":lua require 'harpoon.ui'.nav_file(1)<CR>" },
+        { "<C-t>", ":lua require 'harpoon.ui'.nav_file(2)<CR>" },
+        { "<C-n>", ":lua require 'harpoon.ui'.nav_file(3)<CR>" },
+        { "<C-s>", ":lua require 'harpoon.ui'.nav_file(4)<CR>" },
     },
 }
