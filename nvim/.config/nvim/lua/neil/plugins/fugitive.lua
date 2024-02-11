cf54d78fd7ee35136aac868b571c8af650523412
imneedle <77881374+imneedle@users.noreply.github.com>
2024-02-11 15:37:58 -0500

    feat: added new keymaps to vim fugitive
    - added git pull --rebase
    - added git push
    - added diffget //2 and //3

--- nvim/.config/nvim/lua/neil/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/neil/plugins/fugitive.lua
@@ -2,7 +2,34 @@ return {
     "tpope/vim-fugitive",
     lazy = true,
     cmd = "Git",
-    dependencies = {
-        "lewis6991/gitsigns.nvim",
+    keys = {
+        { "gu", ":diffget //2<CR>" },
+        { "gh", ":diffget //3<CR>" },
     },
+    config = function()
+        local fugitive_group =
+            vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = fugitive_group,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git("push")
+                end, opts)
+
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({ "pull", "--rebase" })
+                end, opts)
+            end,
+        })
+    end,
 }
