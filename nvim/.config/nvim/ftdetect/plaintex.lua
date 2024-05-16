ee84f43cefe87c0f68ef18dd153a419e29e28b96
imneedle <77881374+imneedle@users.noreply.github.com>
2024-05-15 21:53:22 -0400

    fix(nvim): ftdetect using lua callback rather than vim command

--- nvim/.config/nvim/ftdetect/plaintex.lua
+++ nvim/.config/nvim/ftdetect/plaintex.lua
@@ -2,7 +2,9 @@ local group = vim.api.nvim_create_augroup("ftdetectPlaintex", {
     clear = true,
 })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    command = "set filetype=tex",
+    callback = function()
+        vim.bo.filetype = "tex"
+    end,
     pattern = "*.tex",
     group = group,
 })
--- nvim/.config/nvim/ftdetect/tcss.lua
+++ nvim/.config/nvim/ftdetect/tcss.lua
@@ -2,7 +2,9 @@ local group = vim.api.nvim_create_augroup("ftdetectTcss", {
     clear = true,
 })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    command = "set filetype=css",
+    callback = function()
+        vim.bo.filetype = "css"
+    end,
     pattern = "*.tcss",
     group = group,
 })
